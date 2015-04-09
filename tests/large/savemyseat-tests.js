e4e03a86ac44d112f20cb4020528ac76210b2c02
Reuven V. Gonzales <reuven@virtru.com>
2015-04-09 12:56:27 -0700

    Added additional assertions

--- tests/large/savemyseat-tests.js
+++ tests/large/savemyseat-tests.js
@@ -48,7 +48,17 @@ describe('Savemyseat', function() {
     sourceNano = nanoInit(sourceCouchdbUrl);
   });    
 
-  couchdbFixture({}).beforeEach(function(backupFixtureContext) {
+  var backupFixtureData = {};
+
+  backupFixtureData[DB_WITH_OLD_SAVEMYSEAT_DESIGN_DOC_NAME] = [
+    { 
+      _id: '_design/savemyseat',
+      version: '0.0.1'
+    }
+  ];
+
+  couchdbFixture(backupFixtureData).beforeEach(function(backupFixtureContext) {
+  }).beforeEach(function(backupFixtureContext) {
     var backupConfigObj = {};
     backupConfigObj[DB_WITH_DESIGN_DOC_NAME] = {
       source: util.format("%s/%s", sourceCouchdbUrl, DB_WITH_DESIGN_DOC_NAME)
@@ -67,10 +77,14 @@ describe('Savemyseat', function() {
 
   it("should successfully replicate the source server's databases", function(done) {
     var backupDbWithoutDesignDoc = backupNano.use(DB_WITHOUT_DESIGN_DOC_NAME);
+    var backupDbWithDesignDoc = backupNano.use(DB_WITH_DESIGN_DOC_NAME);
+    var backupDbWithOldSavemyseatDesignDoc = backupNano.use(DB_WITH_OLD_SAVEMYSEAT_DESIGN_DOC_NAME);
 
     savemyseat.prepareSourcesForBackup()
       .then(
         function success() {
+          // attempt to retrieve a doc that hasn't been replicated yet on the
+          // backupdb without a design doc
           return Q.nfcall(backupDbWithoutDesignDoc.get, 'some-fake-document');
         }
       )
@@ -84,22 +98,79 @@ describe('Savemyseat', function() {
       )
       .then(
         function success() {
+          // attempt to retrieve a doc that hasn't been replicated yet on the
+          // backupdb with a design doc
+          return Q.nfcall(backupDbWithoutDesignDoc.get, 'some-fake-document');
+        }
+      )
+      .then(
+        function success() {
+          assert.fail(null, null, 'should not succeed in retrieving document before replication');
+        },
+        function error(err) {
+          assert.equal(err.error, 'not_found');
+        }
+      )
+      .then(
+        function success() {
+          // Initiate the replication
           return savemyseat.initializeDatabaseBackups();
         }
       )
       .then(
         function success() {
+          // Wait for replication to complete (.5 seconds for good measure)
           return Q.delay(500);
         }
       )
       .then(
         function success() {
+          // attempt to retrieve the doc now that replication has hopefully completed
           return Q.nfcall(backupDbWithoutDesignDoc.get, 'some-fake-document');
         }
       )
       .then(
         function success(doc) {
+          // check the values in the doc
           assert.equal(doc[0].random, 'stuff1234');
+        }
+      )
+      .then(
+        function success() {
+          // attempt to retrieve the doc now that replication has hopefully completed
+          return Q.nfcall(backupDbWithDesignDoc.get, 'some-fake-document');
+        }
+      )
+      .then(
+        function success(doc) {
+          // check the values in the doc
+          assert.equal(doc[0].random, 'stuff5678');
+        }
+      )
+      .then(
+        function success() {
+          // attempt to retrieve the doc now that replication has hopefully completed
+          return Q.nfcall(backupDbWithDesignDoc.get, '_design/fake-design-doc');
+        }
+      )
+      .then(
+        function success(doc) {
+          // check the values in the doc
+          assert.fail(null, null, 'should not succeed in replicating the design doc from the source');
+        },
+        function error(err) {
+          assert.equal(err.error, 'not_found');
+        }
+      )
+      .then(
+        function success() {
+          // attempt to retrieve the design doc that was 0.0.1 but should be 1.0.0 after initialization
+          return Q.nfcall(backupDbWithOldSavemyseatDesignDoc.get, '_design/savemyseat');
+        }
+      )
+      .then(
+        function success(doc) {
+          assert.equal(doc[0].version, '1.0.0');
         }
       )
       .done(
@@ -109,6 +180,21 @@ describe('Savemyseat', function() {
         function error(err) {
           return done(err);
         }
-      )
+      );
   });
+
+  it('should fail to initialize database backups if the source has not been prepared by savemyseat', function(done) {
+    savemyseat.initializeDatabaseBackups()
+      .done(
+        function success() {
+          assert.fail(null, null, 'should not succeed in initializing the database because the source is not configured correctly');
+        },
+        function error(err) {
+          assert.equal(err.name, 'DatabaseBackupSourceError');
+          return done();
+        }
+      );
+  });
+
+
 });
